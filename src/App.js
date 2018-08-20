62ff5b51e08c2387949220e536f20848089ecf40
zimman78 <zimman78@gmail.com>
2018-08-20 16:08:43 -0400

    initial commit and create App.js

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+import { Header } from './Components/Header';
+import { Textbox } from './Components/Textbox';
+import { DisplayToday, DisplayTomorrow, DisplayDayAfterTomorrow } from './Components/Display';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      'Today': [],
+      'Tomorrow': [],
+      'Day_After_Tomorrow': [],
+      'username': 'yo buddy!'
+    }
+    this.updateEntry = this.updateEntry.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
+  }
+
+  updateEntry(term, day){
+    if (day === 'Today'){
+      this.setState({
+        'Today': [...this.state.Today, term],
+      })
+    }else if (day === 'Tomorrow') {
+      this.setState({
+        'Tomorrow': [...this.state.Tomorrow, term],
+      })
+    }else if (day === 'Day_After_Tomorrow'){
+      this.setState({
+        'Day_After_Tomorrow': [...this.state.Day_After_Tomorrow, term],
+      })
+    }
+  }
+
+  deleteEntry(index, day){
+    if (day === 'Today'){
+      let filterList = this.state.Today.filter((elem, i) => {
+        if (i === index){
+          return false;
+        }
+        return true;
+      })
+      this.setState({
+        'Today': filterList
+      });
+    }else if (day === 'Tomorrow'){
+      let filterList = this.state.Tomorrow.filter((elem, i) => {
+        if (i === index){
+          return false;
+        }
+        return true;
+      });
+      this.setState({
+        'Tomorrow': filterList
+      });
+    }else if (day === 'Day_After_Tomorrow'){
+      let filterList = this.state.Day_After_Tomorrow.filter((elem, i) == {
+        if(i === index){
+          return false;
+        }
+        return true;
+      })
+      this.setState({
+        'Day_After_Tomorrow': filterList
+      });
+    }else{
+      console.log('something went wrong while removing item...');
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <Header nam={this.state.username} />
+        <Textbox updateEntry={this.updateEntry} />
+        <hr />
+        <div className="grid">
+          <DisplayToday items={this.state.Today} deleteItem={this.deleteEntry} />
+          <DisplayTomorrow items={this.state.Tomorrow} deleteItem={this.deleteEntry} />
+          <DisplayDay_After_Tomorrow items={this.state.Day_After_Tomorrow} deleteItem={this.deleteEntry} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
